59595856236d9b50f3af81bcf069f6be697399bf
Chijioke Ibekwe <ibekwe.chijioke18@gmail.com>
2023-05-31 06:03:18 +0100

    test: add test config

--- src/main/java/com/project/workitemprocessor/WorkItemProcessorApplication.java
+++ src/main/java/com/project/workitemprocessor/WorkItemProcessorApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-//@ComponentScan("com.project.workitemprocessor.repository.impl")
+@ComponentScan("com.project.workitemprocessor.repository.impl")
 @SpringBootApplication
 public class WorkItemProcessorApplication {
 
